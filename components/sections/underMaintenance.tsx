5f0b679bb03b4e11afb1c624f2737ed03aeb22dd
zakiezabar <zakiezabar@gmail.com>
2025-04-16 23:31:47 +0800

    updated undermaintenance page

--- components/sections/underMaintenance.tsx
+++ components/sections/underMaintenance.tsx
@@ -3,7 +3,7 @@ export default function UnderMaintenance() {
     <div className="relative w-full mx-auto flex flex-col items-center justify-center bg-secondary-800 py-24 lg:py-52 h-screen">
       <div className="w-full flex flex-col gap-2 text-center">
         <div className="w-full lg:w-1/2 mx-auto text-5xl lg:text-9xl text-primary-100 font-black uppercase">
-          Oo<span className="text-primary-400">pps 🕳️</span>
+          O&apos;o<span className="text-primary-400">oh 🕳️</span>
         </div>
         <div className="text-base lg:text-2xl text-primary-100 font-bold w-full lg:w-1/2 mx-auto">
           <div>This page isn&apos;t ready yet </div>
